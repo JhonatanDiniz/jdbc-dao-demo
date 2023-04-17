b7a85dc1431f228dead09264701f42b4f65269c3
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2023-04-17 08:58:24 -0300

    Criado classe Seller e interfaces Seller e Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Department obj = new Department(1, "Vendas");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(8, "Jhonatan", "Jhonatan@mail.com", sdf.parse("01/06/1993"), 8000.00, obj);
+		
+		System.out.println(seller);
 		
 
 	}

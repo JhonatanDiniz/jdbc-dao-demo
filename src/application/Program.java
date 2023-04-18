6f9c529eb979edc92562f9f678e5d151acca18a8
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2023-04-18 11:22:31 -0300

    Criado metodo findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,30 @@
 package application;
 
 import java.text.ParseException;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		SellerDao sellerDao = DaoFactory.createSeller();
-		
+
 		System.out.println("==========Seller Find by ID ==========");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		System.out.println();
-		
+
+		System.out.println("==========Seller Find by Department Id ==========");
+		Department dep = new Department(1, null);
+		List<Seller> list = sellerDao.findByDepartmentId(dep);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
